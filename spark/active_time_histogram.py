be3b5dbffdc50bb523b707c8df7fdc1a3b293ede
Arutiun Mokoian <arutyunmokoyan@gmail.com>
2018-10-11 07:01:56 +0200

    add article creation time histogram

--- spark/active_time_histogram.py
+++ spark/active_time_histogram.py
@@ -8,12 +8,14 @@ from pyspark.sql.types import IntegerType
 slen = udf(lambda s: len(s), IntegerType())
 
 
-def creation_date(df):
+def creation_date_of_article(df):
     columns_to_drop = ['redirect', 'ns', 'revision', 'date']
     df = df.withColumn("author", df["revision"].getItem(0).contributor.username)\
         .withColumn("authorID", df["revision"].getItem(0).contributor.id)\
         .withColumn("date", df["revision"].getItem(0).timestamp)\
-        .withColumn("creation_date", from_unixtime('date', 'yyyy-MM-dd HH:mm:ss').cast("timestamp"))
+        .withColumn("creation_date_of_article", from_unixtime('date', 'yyyy-MM-dd HH:mm:ss').cast("timestamp"))\
+        .withColumn("time_since_creation",
+                    f.round(months_between(col("creation_date_of_article"), to_timestamp(f.lit("2001-01-15")))))
     df_res = df.drop(*columns_to_drop)
     return df_res
 
@@ -29,7 +31,7 @@ def last_edit_date(df):
 def join_df(df1, df2):
     df_joined = df1.join(df2, "title")\
         .withColumn("active time",
-                    f.round(months_between(col("last_edit_date"), col("creation_date")), -1)) \
+                    f.round(months_between(col("last_edit_date"), col("creation_date_of_article")), -1)) \
         .withColumn("time_since_last_edit",
                     f.round(months_between(to_timestamp(f.lit("2018-05-21")), col("last_edit_date"))))
     return df_joined
@@ -37,12 +39,12 @@ def join_df(df1, df2):
 
 def join_last_and_creation_dates(df):
     df_last_edit_date = last_edit_date(df)
-    df_creation_date = creation_date(df)
+    df_creation_date = creation_date_of_article(df)
     df_res = join_df(df_creation_date, df_last_edit_date)
     return df_res
 
 
-def draw_histogram(df1, df2):
+def draw_histogram(df1, df2, df3):
     fig, axes = plt.subplots(nrows=2, ncols=2)
     fig.set_size_inches(20, 20)
     hist(axes[0, 0], [df1], bins=20, color=['red'])
@@ -53,7 +55,11 @@ def draw_histogram(df1, df2):
     axes[0, 1].set_title('Länge der Artikel-Historie')
     axes[0, 1].set_xlabel('Zeitraum (Anazhl Monaten)')
     axes[0, 1].set_ylabel('Länge (Anzahl Revisionen)')
-    plt.savefig('ActiveTime_TimeSinceLastEdit_Histogram')
+    hist(axes[1, 0], [df3], bins=20, color=['purple'])
+    axes[1, 0].set_title('Erstellungsdatum des Artikels')
+    axes[1, 0].set_xlabel('Zeitraum (Anazhl Monaten)')
+    axes[1, 0].set_ylabel('Anzahl Artikeln')
+    plt.savefig('CreationTime_ActiveTime_TimeSinceLastEdit_Histogram')
 
 
 df = load_to_spark.init()
@@ -61,6 +67,11 @@ df_res = join_last_and_creation_dates(df)
 df_res.show()
 
 # Draw histograms
+df_article_creation = df_res.select(col("time_since_creation")).orderBy(desc("time_since_creation"))
+df_article_creation.cache()
+df_article_creation.persist()
+df_article_creation.show()
+
 df_active_time = df_res.select(col("active time").alias("active")).orderBy(desc("active"))
 df_active_time.cache()
 df_active_time.persist()
@@ -71,7 +82,7 @@ df_since_last_edit.cache()
 df_since_last_edit.persist()
 df_since_last_edit.show()
 
-draw_histogram(df_active_time, df_since_last_edit)
+draw_histogram(df_active_time, df_since_last_edit, df_article_creation)
 
 print('DONE')
 
