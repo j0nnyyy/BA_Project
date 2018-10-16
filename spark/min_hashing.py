ab277257e453a0d0d7decd82405c777ea56b9237
Arutiun Mokoian <arutyunmokoyan@gmail.com>
2018-10-16 02:27:35 +0200

    fix min_hashing function

--- spark/min_hashing.py
+++ spark/min_hashing.py
@@ -1,35 +1,46 @@
-from pyspark.ml.feature import VectorAssembler
-from pyspark.mllib.regression import LabeledPoint
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import MinHashLSH
 from pyspark.sql.functions import desc, asc, col
-from pyspark.sql.functions import udf
 import load_to_spark
 from pyspark.sql.window import Window as W
 import pyspark.sql.functions as f
+import matplotlib.pyplot as plt
+from pyspark_dist_explore import hist
 
 search_text = ['Bot', 'Bots']
+
+
+def draw_histogram(df):
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.set_size_inches(20, 20)
+    hist(axes[0, 0], [df], bins=20, color=['red'])
+    axes[0, 0].set_xlabel('Jaccard Koeffizient')
+    axes[0, 0].set_ylabel('Anzahl der Artikeln')
+    plt.savefig('Jaccard_Similarity')
+
+
+def sparse_vec(r):
+    li = set(r[1])
+    li = sorted(li)
+    l = len(li)
+    vals = [1.0 for x in range(l)]
+    return r[0], Vectors.sparse(942000, li, vals)
 
 df_gn = load_to_spark.main_init_df()
 
 df_titles = df_gn.select("title", "author").where(col("author").isNotNull())
 
-#Determine all distinct authors and generate IDs for each
+# Determine all distinct authors, exclude bots and generate IDs for each
 df_all_authors = df_gn.select("author").where(col("author").isNotNull()).distinct()
-#print(df_all_authors.count())
 
-#Select only Bots
+# Select only Bots
 df_bots = df_all_authors.where(col("author").rlike('|'.join(search_text)))
-#print('Bots count = ', df_bots.count())
 
-#Select all authors except bots
+# Select all authors except bots
 df_real_users = df_all_authors.subtract(df_bots)
-#print('Real users count = ', df_real_users.count())
 
 windowSpec = W.orderBy("author")
-df_authors = df_real_users.withColumn("Id",
-                                      f.row_number().over(windowSpec))
-df_authors.show()
+df_authors = df_real_users.withColumn("Id", f.row_number().over(windowSpec))
 
 print("Join both dataframes by author-column:")
 df1_a = df_titles.alias("df1_a")
@@ -40,22 +51,11 @@ df_joined.show(20)
 
 # create a binary vector
 
-dfWithFeat = df_joined\
-    .rdd\
-    .map(lambda row: (row['title'], row['id'])).groupByKey().toDF()
-    #.map(lambda row: LabeledPoint(row[0], Vectors.sparse(10000, row[1])))\
-    #.toDF()
+dfWithFeat = df_joined.rdd.map(lambda r: (r['title'], (float(r['id'])))).groupByKey()\
+    .map(lambda r: sparse_vec(r)).toDF()
+df_res = dfWithFeat.select(col("_1").alias("title"), col("_2").alias("features"))
+df_res.show()
 
-dfWithFeat.show(2)
-
-
-df_pivoted = df_joined.groupBy("title").pivot("id").count().na.fill(0)
-df_pivoted.show(2)
-
-input_cols = [x for x in df_pivoted.columns if x != "title"]
-
-df_res = (VectorAssembler(inputCols=input_cols, outputCol="features").transform(df_pivoted).select("title", "features"))
-df_res.show(2)
 
 mh = MinHashLSH(inputCol="features", outputCol="hashes", numHashTables=5)
 model = mh.fit(df_res)
@@ -65,7 +65,13 @@ print("The hashed dataset where hashed values are stored in the column 'hashes':
 model.transform(df_res).show()
 
 print("Approximately distance smaller than 0.6:")
-model.approxSimilarityJoin(df_res, df_res, 0.6, distCol="JaccardDistance")\
-    .select(col("datasetA.title").alias("idA"),
-            col("JaccardDistance")).show()
+df_jacc_dist = model.approxSimilarityJoin(df_res, df_res, 0.6, distCol="JaccardDistance")\
+    .select(col("datasetA.title").alias("title"),
+            col("JaccardDistance")).filter("JaccardDistance != 0").orderBy(desc("JaccardDistance"))
+df_jacc_dist.show()
 
+df_hist = df_jacc_dist.select(col("JaccardDistance"))
+
+draw_histogram(df_hist)
+
+print('DONE')
